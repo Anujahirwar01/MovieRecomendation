5d3eb5826e9f4a1ba8911648a577d95483a9b9b9
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-02-27 22:52:17 +0530

    user side done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+// const authMiddleware = require('../middlewares/auth.middleware');
+const blacklisTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res,next) => {
     const errors = validationResult(req);
@@ -19,3 +21,34 @@ module.exports.registerUser = async (req, res,next) => {
         const token = user.generateAuthToken();
         res.status(201).json({ user, token });
 };
+
+module.exports.loginUser = async (req, res,next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email }).select('+password');
+    if (!user) {
+        return res.status(401).json({ errors: [{ msg: 'Invalid email or password' }] });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+        return res.status(401).json({ errors: [{ msg: 'Invalid email or password' }] });
+    }
+    const token = user.generateAuthToken();
+    res.cookie('token', token);
+    res.status(200).json({ user, token });
+    // next();
+    };
+
+module.exports.getUserProfile = async (req, res,next) => {
+    res.status(200).json(req.user);
+};
+    
+module.exports.logoutUser = async (req, res,next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    await blacklisTokenModel.create({ token });
+    res.status(200).json({ message: 'Logged out successfully' });
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router(); // Create a new router using the express.Router
 const {validationResult} = require('express-validator'); // Import the validationResult method from express-validator
 const userController = require('../controllers/user.controller'); // Import the user controller
 const { body } = require('express-validator'); // Import the body method from express-validator
+const authMiddleware = require('../middlewares/auth.middleware'); // Import the auth middleware
 
 router.post('/register', [
   body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
@@ -10,6 +11,16 @@ router.post('/register', [
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ]
 , userController.registerUser); // Create a new user
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+]
+, userController.loginUser); // Login a user
+
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile); // Get user profile
+
+router.get('/logout',authMiddleware.authUser ,userController.logoutUser); // Logout a user
 
 
 
