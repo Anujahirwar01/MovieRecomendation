f90baad17a05418b8b6730e5ea814d1264e90797
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-02-28 23:23:29 +0530

    react part start

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Signup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+            <p>Welcome to the signup page!</p>
+        </div>
+    )
+}
+export default Signup
