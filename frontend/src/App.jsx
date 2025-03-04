b1dea8566427e6dad2c6413c065103046eed59b7
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-03-04 22:45:34 +0530

    l

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
       <Route path='/main' element={<Main/>} />
+      
     </Routes>
     </div>;
 };
